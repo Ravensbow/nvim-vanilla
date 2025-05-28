53889e487a57ae2dcdc7b185200bddcfc0d44bcc
Jakub Walkowski <walkoskuba@gmail.com>
2025-05-28 09:14:01 +0200

    remove ai and blink config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,7 +1,7 @@
 return {
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  dependencies = { 'rafamadriz/friendly-snippets', 'Kaiser-Yang/blink-cmp-avante' },
 
   -- use a release tag to download pre-built binaries
   version = '1.*',
@@ -25,7 +25,7 @@ return {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'default' },
+    keymap = { preset = 'super-tab' },
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
@@ -34,12 +34,28 @@ return {
     },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
+    completion = {
+		ghost_text = {
+			enabled = true,
+		},
+		documentation = {
+			auto_show = false,
+		},
+	},
 
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+	-- Default list of enabled providers defined so that you can extend it
+	-- elsewhere in your config, without redefining it, due to `opts_extend`
+	sources = {
+		default = { 'avante', 'lsp', 'path', 'snippets', 'buffer' },
+		providers = {
+			avante = {
+				module = 'blink-cmp-avante',
+				name = 'Avante',
+				opts = {
+					-- options for blink-cmp-avante
+				}
+			}
+		},
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
